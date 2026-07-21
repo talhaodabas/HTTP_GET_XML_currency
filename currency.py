e3fe8dc9e9c0b50c4827fc0644d33f6e7ed06a9d
talhaodabas <mustafatalhaodabas170@gmail.com>
2026-07-22 01:19:17 +0300

    refactor-function ve mini düzeltmeler eklendi

--- currency.py
+++ currency.py
@@ -1,42 +1,77 @@
 import xml.etree.ElementTree as ET
 from urllib.request import urlopen
 
-try:
+def get_xml(url):
+    try:
+        with urlopen(url) as response:
+                xml_data = response.read()
+        return xml_data
+    except Exception as e:
+        print(f"Bir hata oluştu: {e}")
 
-    while True:
-        
-        with urlopen("https://www.tcmb.gov.tr/kurlar/today.xml") as response:
-            xml_data = response.read()
-        root = ET.fromstring(xml_data)
+def set_xml():
+    try:
+        root = ET.fromstring(get_xml("https://www.tcmb.gov.tr/kurlar/today.xml"))
         currencys =[]
         for x,currency in enumerate(root.findall("Currency"), start=1):
             CurrencyCode = currency.get("CurrencyCode")
             Isim = currency.find("Isim").text
-            ForexBuying = currency.find("ForexBuying").text
-            ForexSelling = currency.find("ForexSelling").text
-            BanknoteBuying = currency.find("BanknoteBuying").text
-            BanknoteSelling = currency.find("BanknoteSelling").text
-            CrossRateUSD = currency.find("CrossRateUSD").text
-            CrossRateOther = currency.find("CrossRateOther").text
-            currencys.append([x,CurrencyCode,Isim,ForexBuying,ForexSelling,BanknoteBuying,BanknoteSelling,CrossRateUSD,CrossRateOther])
-            print(f"{currencys[x-1][0]} - {currencys[x-1][1]} ({currencys[x-1][2]})")
+            ForexBuying = safe_float(currency.find("ForexBuying"))
+            ForexSelling = safe_float(currency.find("ForexSelling"))
+            BanknoteBuying = safe_float(currency.find("BanknoteBuying"))
+            BanknoteSelling = safe_float(currency.find("BanknoteSelling"))
+            Unit = currency.find("Unit").text
+
+
+            currencys.append([x,CurrencyCode,Isim,ForexBuying,ForexSelling,BanknoteBuying,BanknoteSelling,Unit])
+        return currencys
+    except Exception as e:
+            print(f"Bir hata oluştu: {e}")
+
+def print_xml(currencys):
+    try:
+        for currency in currencys:
+             print(f"{currency[0]} - {currency[1]} ({currency[2]})")
         print()
-        s_currency = int(input("Bir numara secin: ")) - 1
-        if s_currency > (len(currencys)-1):
+    except Exception as e:
+                print(f"Bir hata oluştu: {e}")
+
+def safe_float(element):
+    if element is not None and element.text:
+        return  float(element.text)
+    return 0.0
+
+def calculate_currency(currency):
+    try:
+        money = float(input("Döviz islemi icin belirlediginiz miktar(tl) : "))
+        if currency[7] == "100":
+             finall_money =  (money / (currency[3]))*100
+             print(f"{money} TL = {format(finall_money, '.3f')} {currency[1]}")
+        else:     
+            finall_money =  money / (currency[3])
+            print(f"{money} TL = {format(finall_money, '.3f')} {currency[1]}")
+    except Exception as e:
+        print(f"Bir hata oluştu: {e}")
+
+try:
+    while True:
+        currencys = set_xml()
+        print_xml(currencys)
+
+        currency = int(input("Bir numara secin: ")) - 1
+        if currency > (len(currencys)-1):
             print("Yanlis tuslama")
             break
-        print(f"{currencys[s_currency][1]}, Döviz Alis/Satis {currencys[s_currency][3]}/{currencys[s_currency][4]}, Efektif Alis/Satis {currencys[s_currency][5]}/{currencys[s_currency][6]}, CrossRateUSD {currencys[s_currency][7]}, CrossRateOther {currencys[s_currency][8]}")
+        currency = currencys[currency]
+        print(f"{currency[1]}, Döviz Alis/Satis = {currency[3]}/{currency[4]}, Efektif Alis/Satis = {currency[5]}/{currency[6]}")
         
         while True:
             select = int(input("Ana menü için 0 Belirli bir para miktarini çevirmek için 1: "))
             if select == 1:
-                money = float(input("Döviz islemi icin belirlediginiz miktar(tl) : "))
-                finall_money =  money / float(currencys[s_currency][3])
-                print(f"{money} TL = {format(finall_money, '.3f')} {currencys[s_currency][1]}")
+                calculate_currency(currency)
             elif select == 0:
                 break
             else:
                 print("Yanlis tuslama")
-
 except Exception as e:
     print(f"Bir hata oluştu: {e}")
